df7a0e4b563fc129585e499d979f59cefd739836
ivancas84 <ivancas84@gmail.com>
2023-04-24 22:13:41 -0300

    Pruebas iniciales

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ sys.path.insert(0, "../")
 
 from py_sqlo.src.db import Db
 from py_sqlo.src.config import APPROX, EQUAL, NONEQUAL
+from py_sqlo.src.function.to_bool import to_bool
 
 
 config = {
@@ -16,14 +17,32 @@ config = {
 }
 
 db = Db(config)
-q = db.query("persona").cond(("nombres",APPROX,"Ivan")).sql()
+q = db.query("persona").cond(
+    ("nombres",EQUAL,("Ivan", "Juan"))
+).sql()
 print(q)
-# mycursor = db.conn().cursor()
+# cursor = db.conn().cursor()
 
-# sql = "SELECT * FROM persona WHERE nombres LIKE %s"
+# sql = "SELECT tiene_constancia FROM alumno WHERE tiene_constancia = true LIMIT 1 "
 
-# mycursor.execute(sql, ("%"+"Juan"+"%", ))
+# cursor.execute(sql)
+# # remaining_rows = cursor.fetchall()
+# # print(remaining_rows)
 
-# print(mycursor.statement)
+
+# class CursorByName():
+#     def __init__(self, cursor):
+#         self._cursor = cursor
+    
+#     def __iter__(self):
+#         return self
+
+#     def __next__(self):
+#         row = self._cursor.__next__()
+
+#         return { description[0]: row[col] for col, description in enumerate(self._cursor.description) }
+    
+# for row in CursorByName(cursor):
+#     print(to_bool(row["tiene_constancia"]))
 
 
