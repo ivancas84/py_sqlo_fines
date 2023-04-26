728bfe1aaee16f55c84e33309b437a6b4dd2729a
ivancas84 <ivancas84@gmail.com>
2023-04-26 10:29:00 -0300

    Desarrollo inicial

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ config = {
 
 db = Db(config)
 q = db.query("persona").cond(
-    ("nombres",EQUAL,("Ivan", "Juan"))
+    ("nombres","EQUAL",("Ivan", "Juan"))
 ).sql()
 print(q)
 # cursor = db.conn().cursor()
