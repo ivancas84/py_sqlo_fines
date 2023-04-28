ba48ec52abfa2aaaae9de37bb57031cd4921bebe
ivancas84 <ivancas84@gmail.com>
2023-04-28 12:06:00 -0300

    Pruebas

--- test/test.py
+++ test/test.py
@@ -2,43 +2,40 @@ import sys
 import datetime
 from dateutil.parser import parse
 
+
 sys.path.insert(0, "../")
 
-# from py_sqlo.src.db import Db
+from py_sqlo.src.db import Db
 
 
-# config = {
-#     "user":"root",
-#     "password":"",
-#     "host":"localhost",
-#     "database":"planfi10_20203",
-#     "path_model":"C:\\xampp\\htdocs\\py_sqlo_fines\\model\\",
-#     "driver":"mysql"
-# }
-d = parse("2015-02-24")
-dd = d.date()
-print(type(dd))
-print(dd)
+config = {
+    "user":"root",
+    "password":"",
+    "host":"localhost",
+    "database":"planfi10_20203",
+    "path_model":"C:\\xampp\\htdocs\\py_sqlo_fines\\model\\",
+    "driver":"mysql"
+}
+d = parse("-")
+print(type(d))
+print(d)
 
-d = datetime.datetime.strptime("2012-04-23", '%Y-%m-%d')
+# d = datetime.datetime.strptime("2012-04-23", '%Y-%m-%d')
 
+db = Db(config)
 
-
-
-
-# db = Db(config)
 # q = db.query("persona").cond((
 #     ("persona-fecha_nacimiento","EQUAL","2021"),
 # )).sql()
 # print(q)
-# cursor = db.conn().cursor()
+cursor = db.conn().cursor()
 
-# sql = "SELECT tiene_constancia FROM alumno WHERE tiene_constancia = true LIMIT 1 "
+sql = "SELECT anio FROM calendario WHERE anio = %s"
 
-# cursor.execute(sql)
-# # remaining_rows = cursor.fetchall()
-# # print(remaining_rows)
-
+cursor.execute(sql, ("2022",))
+remaining_rows = cursor.fetchall()
+print(remaining_rows)
+print(type(remaining_rows[0][0]))
 
 # class CursorByName():
 #     def __init__(self, cursor):
