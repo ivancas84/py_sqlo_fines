a61324ac625554fe646c48ff9faaa861a1a0d1a6
unknown <ivancas84@gmail.com>
2023-05-01 10:06:52 -0300

    Pruebas iniciales

--- test/test.py
+++ test/test.py
@@ -16,26 +16,23 @@ config = {
     "path_model":"C:\\xampp\\htdocs\\py_sqlo_fines\\model\\",
     "driver":"mysql"
 }
-d = parse("-")
-print(type(d))
-print(d)
+# d = parse("-")
+# print(type(d))
+# print(d)
 
 # d = datetime.datetime.strptime("2012-04-23", '%Y-%m-%d')
 
 db = Db(config)
 
-# q = db.query("persona").cond((
-#     ("persona-fecha_nacimiento","EQUAL","2021"),
-# )).sql()
-# print(q)
-cursor = db.conn().cursor()
+q = db.query("persona").fields().order({"fecha_nacimiento":"DESC", "apellidos":"DESC"}).page(4).size(10).sql()
+# cursor = db.conn().cursor()
 
-sql = "SELECT anio FROM calendario WHERE anio = %s"
+# sql = "SELECT anio FROM calendario WHERE anio = %s"
 
-cursor.execute(sql, ("2022",))
-remaining_rows = cursor.fetchall()
-print(remaining_rows)
-print(type(remaining_rows[0][0]))
+# cursor.execute(sql, ("2022",))
+# remaining_rows = cursor.fetchall()
+# print(remaining_rows)
+# print(type(remaining_rows[0][0]))
 
 # class CursorByName():
 #     def __init__(self, cursor):
